718975b15814bf172bc57120d0573cce264f25f4
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-05-14 19:10:31 +0530

    sanity init

---
+++ client/src/client.js
@@ -0,0 +1,6 @@
+import {createClient} from '@sanity/client'
+
+export default createClient({
+	projectId:"xle18hlu",
+	dataset:"production"
+})
---
+++ ecom/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'xle18hlu',
+    dataset: 'production'
+  }
+})
